0b3c258d59923c0f535dc9007ac3a100aacf2193
topooh <38042960+topooh@users.noreply.github.com>
2023-02-08 11:37:17 -0300

    Update index.php
    se ven los datos de los empleados, pero aun no permido editar o eliminar estos mismos.

--- secciones/empleados/index.php
+++ secciones/empleados/index.php
@@ -1,3 +1,16 @@
+<?php
+include ("../../bd.php");
+
+
+$sentencia=$conexion->prepare("SELECT *,
+(SELECT nombredelpuesto
+FROM tbl_puestos
+WHERE tbl_puestos.id=tbl_empleados.idpuesto limit 1) as puesto
+from tbl_empleados");
+$sentencia ->execute();
+$lista_tbl_empleados=$sentencia->fetchALL(PDO::FETCH_ASSOC);
+
+?>
 <?php include("../../templates/header.php"); ?>
 
 <center><h4>Personal</h4></center>
@@ -12,6 +25,7 @@
         <table class="table">
             <thead>
                 <tr>
+                    <th scope="col">ID</th>
                     <th scope="col">Nombre</th>
                     <th scope="col">Foto</th>
                     <th scope="col">CV</th>
@@ -21,15 +35,22 @@
                 </tr>
             </thead>
             <tbody>
+
+            <?php foreach($lista_tbl_empleados as $registro){?>
                 <tr class="">
-                    <td scope="row">Matias Muñoz</td>
-                    <td>Foto</td>
-                    <td>CV</td>
-                    <td>Puesto</td>
-                    <td>06-02-20223</td>
+                    <td><?php echo $registro['id']; ?></td>
+                    <td scope="row">
+                     <?php echo $registro['primernombre']; ?>
+                     <?php echo $registro['segundonombre']; ?>
+                     <?php echo $registro['primerapellido']; ?> 
+                     <?php echo $registro['segundoapellido']; ?></td>
+                    <td><?php echo $registro['foto']; ?></td>
+                    <td><?php echo $registro['cv']; ?></td>
+                    <td><?php echo $registro['puesto']; ?></td>
+                    <td><?php echo $registro['fechaingreso']; ?></td>
                     <td><a name="" id="" class="btn btn-primary" href="#" role="button">Decreto</a> | <a name="" id="" class="btn btn-info" href="#" role="button">Editar</a>  | <a name="" id="" class="btn btn-danger" href="#" role="button">Eliminar</a> </td>
                 </tr>
-               
+                <?php } ?> 
                 
             </tbody>
         </table>
